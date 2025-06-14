b0918fef483c0dee164e8685def25bbcc7c0e97f
Aayush Soni <soniaayush5562@gmail.com>
2025-06-14 22:22:42 +0530

    something

--- src/components/product/Products.jsx
+++ src/components/product/Products.jsx
@@ -15,172 +15,115 @@ import Sidebar from "react-sidebar";
 import Tooltip from "@mui/material/Tooltip";
 
 const categories = [
-    "Laptop",
-    "Footwear",
-    "Bottom",
-    "Tops",
-    "Attire",
-    "Camera",
-    "SmartPhones",
+  "Laptop",
+  "Footwear",
+  "Bottom",
+  "Tops",
+  "Attire",
+  "Camera",
+  "SmartPhones",
 ];
 
 function ValueLabelComponent(props) {
-    const { children, open, value } = props;
-    return (
-        <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
-            {children}
-        </Tooltip>
-    );
+  const { children, open, value } = props;
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
 }
 
 const Products = () => {
-    const dispatch = useDispatch();
-    const [currentPage, setCurrentPage] = useState(1);
-    const [price, setPrice] = useState([0, 25000]);
-    const [category, setCategory] = useState("");
-    const [ratings, setRatings] = useState(0);
-    const [sidebarOpen, setSidebarOpen] = useState(false); // State for sidebar visibility
+  const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
+  const [ratings, setRatings] = useState(0);
+  const [sidebarOpen, setSidebarOpen] = useState(false); // State for sidebar visibility
 
-    const priceHandler = (event, newPrice) => {
-        setPrice(newPrice);
-    };
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
 
-    const setCurrentPageNo = (e) => {
-        setCurrentPage(e);
-    };
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
 
-    const {
-        products,
-        productsCount,
-        loading,
-        error,
-        resultPerPage,
-        filteredProductsCount,
-    } = useSelector((state) => state.AllProducts);
+  const {
+    products,
+    productsCount,
+    loading,
+    error,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.AllProducts);
 
-    const keyword = useParams().keyword;
+  const keyword = useParams().keyword;
 
-    useEffect(() => {
-        if (error) {
-            dispatch(ClearErrors());
-            toast.error(error);
-        }
-        dispatch(GetProduct(keyword, currentPage, price, category, ratings));
-    }, [dispatch, keyword, currentPage, price, category, ratings]);
+  useEffect(() => {
+    if (error) {
+      dispatch(ClearErrors());
+      toast.error(error);
+    }
+    dispatch(GetProduct(keyword, currentPage, price, category, ratings));
+  }, [dispatch, keyword, currentPage, price, category, ratings]);
 
-    const toggleSidebar = () => {
-        setSidebarOpen(!sidebarOpen);
-        if (!sidebarOpen) {
-            window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top when opening sidebar
-        }
-    };
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+    if (!sidebarOpen) {
+      window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top when opening sidebar
+    }
+  };
 
-    const handleCategoryClick = (cat) => {
-        if (category === cat) {
-            // If the clicked category is already selected, unselect it
-            setCategory("");
-        } else {
-            // Otherwise, select the clicked category
-            setCategory(cat);
-        }
-    };
+  const handleCategoryClick = (cat) => {
+    if (category === cat) {
+      // If the clicked category is already selected, unselect it
+      setCategory("");
+    } else {
+      // Otherwise, select the clicked category
+      setCategory(cat);
+    }
+  };
 
-    return (
-        <>
-            <MetaData title="PRODUCTS -- ECOMMERCE" />
-            <h2 className="productsHeading">Products</h2>
+  return (
+    <>
+      <MetaData title="PRODUCTS -- ECOMMERCE" />
+      <h2 className="productsHeading">Products</h2>
 
-            {loading ? (
-                <ProductLoaderContainer />
-            ) : (
-                <div className="products">
-                    {products &&
-                        products.map((product) => (
-                            <ProductCard key={product._id} product={product} />
-                        ))}
-                </div>
-            )}
+      {loading ? (
+        <ProductLoaderContainer />
+      ) : (
+        <div className="products">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+        </div>
+      )}
 
-            {/* Toggle Button for Filter Sidebar */}
-            <button className="filterButton" onClick={toggleSidebar}>
-                Filter
-            </button>
+      {/* here it wass */}
 
-            {/* Sidebar Component for Filter Options */}
-            <Sidebar
-                sidebar={
-                    <div className="filterContent">
-                        <Typography>Price Range</Typography>
-                        <Slider
-                            value={price}
-                            onChange={priceHandler}
-                            valueLabelDisplay="auto"
-                            aria-labelledby="range-slider"
-                            min={0}
-                            max={25000}
-                            ValueLabelComponent={ValueLabelComponent}
-                        />
-
-                        <Typography>Categories</Typography>
-                        <ul className="categoryBox">
-                            {categories.map((cat) => (
-                                <li
-                                    className={`category-link ${
-                                        category === cat ? "active" : ""
-                                    }`}
-                                    key={cat}
-                                    onClick={() => handleCategoryClick(cat)}
-                                >
-                                    {cat}
-                                </li>
-                            ))}
-                        </ul>
-
-                        <fieldset>
-                            <Typography component="legend">
-                                Ratings Above
-                            </Typography>
-                            <Slider
-                                value={ratings}
-                                onChange={(e, newRating) =>
-                                    setRatings(newRating)
-                                }
-                                aria-labelledby="continuous-slider"
-                                valueLabelDisplay="auto"
-                                min={0}
-                                max={5}
-                            />
-                        </fieldset>
-                    </div>
-                }
-                open={sidebarOpen}
-                onSetOpen={toggleSidebar}
-                styles={{ sidebar: { background: "white", width: "300px" } }}
-            >
-                {/* Main Content */}
-            </Sidebar>
-
-            {resultPerPage < filteredProductsCount && (
-                <div className="paginationBox">
-                    <Pagination
-                        activePage={currentPage}
-                        itemsCountPerPage={resultPerPage}
-                        totalItemsCount={productsCount}
-                        onChange={setCurrentPageNo}
-                        nextPageText="Next"
-                        prevPageText="Prev"
-                        firstPageText="1st"
-                        lastPageText="Last"
-                        itemClass="page-item"
-                        linkClass="page-link"
-                        activeClass="pageItemActive"
-                        activeLinkClass="pageLinkActive"
-                    />
-                </div>
-            )}
-            <ToastContainer />
-        </>
-    );
+      {resultPerPage < filteredProductsCount && (
+        <div className="paginationBox">
+          <Pagination
+            activePage={currentPage}
+            itemsCountPerPage={resultPerPage}
+            totalItemsCount={productsCount}
+            onChange={setCurrentPageNo}
+            nextPageText="Next"
+            prevPageText="Prev"
+            firstPageText="1st"
+            lastPageText="Last"
+            itemClass="page-item"
+            linkClass="page-link"
+            activeClass="pageItemActive"
+            activeLinkClass="pageLinkActive"
+          />
+        </div>
+      )}
+      <ToastContainer />
+    </>
+  );
 };
 
 export default Products;
